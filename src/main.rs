1168bb3a3baeea54701d68e5eb529bc24c91b5ea
Jesse Ashmore <jesse.ashmore97@gmail.com>
2026-02-22 12:12:55 +0000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,9 @@ use std::io::{self, Write};
 fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut command;
+    io::stdin().read_line(&mut command).unwrap();
+
+    println!("{command}: command not found");
 }

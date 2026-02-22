3edc7855be954e6d840b5eb9c132b5de7dc57d8c
Jesse Ashmore <jesse.ashmore97@gmail.com>
2026-02-22 12:16:51 +0000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,12 @@ fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
 
+    let command = read_input();
+    println!("{}: command not found", command.trim());
+}
+
+fn read_input() -> String {
     let mut command = String::new();
     io::stdin().read_line(&mut command).unwrap();
-
-    println!("{}: command not found", command.trim());
+    command.trim().to_string()
 }

35061ef686ce82d75bdad50bca2e24a41113b1d6
Jesse Ashmore <jesse.ashmore97@gmail.com>
2026-02-24 22:55:22 +0000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+struct Args(Vec<String>);
+
 fn main() {
-    loop {
-        let command = prompt();
-        println!("{}: command not found", command.trim());
+    while let Some(command) = evaluate(&read()) {
         io::stdout().flush().unwrap();
     }
 }
 
-fn prompt() -> String {
+fn read() -> String {
     print!("$ ");
     io::stdout().flush().unwrap();
     let mut command = String::new();
     io::stdin().read_line(&mut command).unwrap();
     command.trim().to_string()
+}
+
+fn evaluate(command: &str) -> Option<impl Fn()> {
+    return match command {
+        "exit" => None,
+        other => Some(|| not_found(other)),
+    };
+}
+
+fn not_found(command: &str) {
+    println!("{}: command not found", command.trim());
 }

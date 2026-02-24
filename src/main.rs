4a02ed5ac6d9ef45a0ec040575265f0fb9309592
Jesse Ashmore <jesse.ashmore97@gmail.com>
2026-02-24 23:34:50 +0000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::vec;
 
+const EMPTY_STRING: String = String::new();
 struct Args(Vec<String>);
 trait Command {
     fn evaluate(&self, args: Args) -> Option<String>;
 }
 enum Cmd {
+    Noop,
     Exit,
     NotFound(String),
+    Echo,
 }
 
 impl Command for Cmd {
@@ -15,6 +19,8 @@ impl Command for Cmd {
         match self {
             Cmd::Exit => None,
             Cmd::NotFound(cmd) => Some(format!("{}: command not found", cmd)),
+            Cmd::Echo => Some(args.0.join(" ")),
+            Cmd::Noop => Some(EMPTY_STRING),
         }
     }
 }
@@ -35,15 +41,24 @@ fn main() {
 fn read() -> (Cmd, Args) {
     print!("$ ");
     io::stdout().flush().unwrap();
-    let mut command = String::new();
-    io::stdin().read_line(&mut command).unwrap();
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let mut parts = input.trim().split(" ");
 
-    let func = match command.trim() {
-        "exit" => Cmd::Exit,
-        other => Cmd::NotFound(other.to_string()),
-    };
+    if let Some(command) = parts.next() {
+        let func = match command.trim() {
+            "exit" => Cmd::Exit,
+            "echo" => Cmd::Echo,
+            other => Cmd::NotFound(other.to_string()),
+        };
 
-    let args = Args(vec![]);
+        let args: Vec<String> = match func {
+            Cmd::Echo => parts.map(|s| s.to_string()).collect(),
+            _ => vec![],
+        };
 
-    (func, args)
+        return (func, Args(args));
+    }
+
+    (Cmd::Noop, Args(vec![]))
 }

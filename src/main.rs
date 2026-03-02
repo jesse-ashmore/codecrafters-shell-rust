d70853e53db133a76066fff55d7c51a1b1a91681
Jesse Ashmore <jesse.ashmore97@gmail.com>
2026-03-02 23:27:28 +0000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
-use std::vec;
+use std::{
+    env, os,
+    path::{self, Path, PathBuf},
+    vec,
+};
+
+use itertools::Itertools;
 
 const EMPTY_STRING: String = String::new();
 struct Args(Vec<String>);
 trait Command {
-    fn evaluate(&self, args: Args) -> Option<String>;
+    fn evaluate(&self, env: &Environment, args: Args) -> Option<String>;
 }
 
 #[derive(PartialEq, Eq)]
@@ -15,7 +21,7 @@ enum Cmd {
     NotFound(String),
     Echo(String),
     Type(String),
-    External(String, String),
+    External(String, PathBuf),
 }
 
 impl Cmd {
@@ -40,34 +46,24 @@ impl Cmd {
 }
 
 impl Command for Cmd {
-    fn evaluate(&self, args: Args) -> Option<String> {
+    fn evaluate(&self, env: &Environment, args: Args) -> Option<String> {
         match self {
             Cmd::Exit(_) => None,
             Cmd::NotFound(cmd) => Some(format!("{}: command not found", cmd)),
             Cmd::Echo(_) => Some(args.0.join(" ")),
             Cmd::Noop => Some(EMPTY_STRING),
-            Cmd::Type(_) => builtin_type(args),
+            Cmd::Type(_) => env.builtin_type(args),
             Cmd::External(_, _) => todo!("Must implement evaluate for Cmd::External"),
         }
     }
 }
 
-fn builtin_type(args: Args) -> Option<String> {
-    let interpret = parse_cmd(&args.0.join(" "));
-
-    match interpret.0 {
-        Cmd::Noop => Cmd::NotFound(EMPTY_STRING).evaluate(args),
-        Cmd::NotFound(name) => Some(format!("{name}: not found")),
-        Cmd::External(name, path) => Some(format!("{name}: is {path}")),
-        _ => Some(format!("{} is a shell builtin", &interpret.0.get_name())),
-    }
-}
-
 fn main() {
     loop {
-        let (cmd, args) = read();
+        let env = Environment::new();
+        let (cmd, args) = env.read();
 
-        if let Some(output) = cmd.evaluate(args) {
+        if let Some(output) = cmd.evaluate(&env, args) {
             println!("{output}");
             io::stdout().flush().unwrap();
         } else {
@@ -76,43 +72,70 @@ fn main() {
     }
 }
 
-fn read() -> (Cmd, Args) {
-    print!("$ ");
-    io::stdout().flush().unwrap();
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    parse_cmd(&input)
-}
-
-fn parse_cmd(input: &str) -> (Cmd, Args) {
-    let mut parts = input.trim().split(" ");
-
-    if let Some(command) = parts.next() {
-        let name = command.trim();
-        let func = Cmd::from_name(name);
-
-        if let Some(builtin) = func {
-            return (builtin, Args(parts.map(|s| s.to_string()).collect()));
-        }
-
-        if let Some(path) = get_path(name) {
-            return (
-                Cmd::External(name.to_string(), path),
-                Args(parts.map(|s| s.to_string()).collect()),
-            );
-        }
-
-        return (Cmd::NotFound(name.to_string()), Args(vec![]));
+fn get_paths() -> Option<Vec<PathBuf>> {
+    let key = "PATH";
+    if let Some(paths) = env::var_os(key) {
+        return Some(env::split_paths(&paths).collect_vec());
     }
 
-    (Cmd::Noop, Args(vec![]))
+    println!("{key} is not defined in the environment.");
+    None
 }
 
-fn get_path(cmd: &str) -> Option<String> {
-    todo!("If the command is not a builtin, your shell must go through every directory in PATH. For each directory:
+struct Environment {
+    paths: Vec<PathBuf>,
+}
 
-    Check if a file with the command name exists.
-    Check if the file has execute permissions.
-    If the file exists and has execute permissions, print <command> is <full_path> and stop.
-    If the file exists but lacks execute permissions, skip it and continue to the next directory.")
+impl Environment {
+    fn read(&self) -> (Cmd, Args) {
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        self.parse_cmd(&input)
+    }
+
+    fn parse_cmd(&self, input: &str) -> (Cmd, Args) {
+        let mut parts = input.trim().split(" ");
+
+        if let Some(command) = parts.next() {
+            let name = command.trim();
+            let func = Cmd::from_name(name);
+
+            if let Some(builtin) = func {
+                return (builtin, Args(parts.map(|s| s.to_string()).collect()));
+            }
+
+            if let Some(found_in_path) = self.paths.iter().find(|path| match path.file_name() {
+                Some(path_name) => name == path_name,
+                None => todo!(),
+            }) {
+                return (
+                    Cmd::External(name.to_string(), found_in_path.clone()),
+                    Args(parts.map(|s| s.to_string()).collect()),
+                );
+            }
+
+            return (Cmd::NotFound(name.to_string()), Args(vec![]));
+        }
+
+        (Cmd::Noop, Args(vec![]))
+    }
+
+    fn builtin_type(&self, args: Args) -> Option<String> {
+        let interpret = self.parse_cmd(&args.0.join(" "));
+
+        match interpret.0 {
+            Cmd::Noop => Cmd::NotFound(EMPTY_STRING).evaluate(&self, args),
+            Cmd::NotFound(name) => Some(format!("{name}: not found")),
+            Cmd::External(name, path) => Some(format!("{name}: is {}", path.to_str().unwrap())),
+            _ => Some(format!("{} is a shell builtin", &interpret.0.get_name())),
+        }
+    }
+
+    fn new() -> Self {
+        Environment {
+            paths: get_paths().unwrap_or(vec![]),
+        }
+    }
 }
